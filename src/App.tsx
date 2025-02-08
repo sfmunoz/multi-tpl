966c7f42aeafe4e2328b1fbdf9966ef8355ca3ce
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-02-08 17:47:47 +0000

    'src/App.tsx' shows a single button now

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <div className="flex flew-row">
-        <p className="m-2 p-3 rounded-xl text-white bg-slate-600">
-          Simple empty page
-        </p>
-      </div>
+      <Button>Button</Button>
     </>
   );
 }
