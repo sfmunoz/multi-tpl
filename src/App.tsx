9998f09891ac736525f67e69b12e682bc866a3ad
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-02-08 15:38:38 +0000

    'src/App.tsx' shows a calendar managed by a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import {
+  Calendar,
+  CalendarCell,
+  CalendarGrid,
+  CalendarGridBody,
+  CalendarGridHeader,
+  CalendarHeaderCell,
+  CalendarHeading,
+} from "@/components/ui/calendar";
+
+const MyCalendar = () => {
+  return (
+    <Calendar aria-label="Appointment date">
+      <CalendarHeading />
+      <CalendarGrid>
+        <CalendarGridHeader>
+          {(day) => <CalendarHeaderCell>{day}</CalendarHeaderCell>}
+        </CalendarGridHeader>
+        <CalendarGridBody>
+          {(date) => <CalendarCell date={date} />}
+        </CalendarGridBody>
+      </CalendarGrid>
+    </Calendar>
+  );
+};
 
 function App() {
+  const [visible, setVisible] = useState(true);
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div className="flex flex-col m-3 gap-3 w-72">
+      <Button
+        variant={visible ? "destructive" : "default"}
+        onPress={() => setVisible((x) => !x)}
+      >
+        {visible ? "Hide" : "Show"} calendar
+      </Button>
+      {visible && <MyCalendar />}
+    </div>
   );
 }
 
