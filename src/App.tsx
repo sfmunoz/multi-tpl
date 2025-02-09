e2f249d25fe397bf809c14ead2118ed8bc0b2884
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-02-09 15:34:32 +0000

    'src/App.tsx' shows an unstyled tab list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Tabs } from "@mantine/core";
+
+function Demo() {
+  return (
+    <Tabs defaultValue="chat" unstyled>
+      <Tabs.List>
+        <Tabs.Tab value="chat">Chat</Tabs.Tab>
+        <Tabs.Tab value="gallery">Gallery</Tabs.Tab>
+        <Tabs.Tab value="account">Account</Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Panel value="chat">Chat panel</Tabs.Panel>
+      <Tabs.Panel value="gallery">Gallery panel</Tabs.Panel>
+      <Tabs.Panel value="account">Account panel</Tabs.Panel>
+    </Tabs>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h2>Simple empty page</h2>
+      <Demo />
     </div>
   );
 }
