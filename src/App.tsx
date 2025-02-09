63f38f13a1447469d082697493465e40f8620e90
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-02-09 15:13:18 +0000

    'src/App.tsx' shows an array of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div>
-      <h2>Simple empty page</h2>
+    <div className="flex flex-row m-3 gap-3">
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </div>
   );
 }
