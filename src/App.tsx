b93be40e67b5f87b818f21bf1895f4007e5bb0bc
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-02-10 18:16:37 +0000

    'src/App.tsx' shows a row of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
+
 function App() {
   return (
-    <div>
-      <h2>Simple empty page</h2>
-    </div>
+    <Stack direction="horizontal" gap={3}>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </Stack>
   );
 }
 
